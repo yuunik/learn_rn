b6862abb64c03a69cfd596cb4229fef6ce51951a
zhou <zhou.z@proteus-dt.com>
2025-09-30 12:01:49 +0800

    feat: 学习 expo 的布局文件

---
+++ app/_layout.jsx
@@ -0,0 +1,35 @@
+import {Slot, Stack} from "expo-router";
+import {Text, StyleSheet} from "react-native";
+import {SafeAreaView} from "react-native-safe-area-context";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#F5F5F5'
+    },
+    header: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        color: '#000',
+        textAlign: 'center',
+        margin: 20
+    },
+    footer: {
+        fontSize: 12,
+        color: '#000',
+        textAlign: 'center',
+        margin: 20
+    }
+})
+
+const Layout = () => {
+    return (
+        <SafeAreaView style={styles.container} >
+            <Text style={styles.header}>React Native</Text>
+            <Slot />
+            <Text style={styles.footer}>@ develop with yuunik</Text>
+        </SafeAreaView>
+    )
+}
+
+export default Layout
--- app/index.jsx
+++ app/index.jsx
@@ -6,13 +6,15 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#f5f5f5',
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        gap: 12,
     },
     textDisPlay: {
         fontSize: 20,
         width: 200,
         fontWeight: 'bold',
-        color: '#e29447'
+        color: '#e29447',
+        textAlign: 'center'
     },
     button: {
         backgroundColor: '#e29447',
